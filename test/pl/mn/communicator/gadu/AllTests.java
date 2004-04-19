cd3c3d98e87b6a74b258f79e7c247c62941e3b05
Marcin Naglik <mnaglik@gazeta.pl>
2004-04-19 15:07:10 +0000

    formatowanie kodu testow

--- test/pl/mn/communicator/gadu/AllTests.java
+++ test/pl/mn/communicator/gadu/AllTests.java
@@ -3,16 +3,18 @@ package pl.mn.communicator.gadu;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+
 /**
  * @author mnaglik
  */
 public class AllTests {
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for pl.mn.communicator.gadu");
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for pl.mn.communicator.gadu");
-		//$JUnit-BEGIN$
-		suite.addTestSuite(GGConversionTest.class);
-		//$JUnit-END$
-		return suite;
-	}
+        //$JUnit-BEGIN$
+        suite.addTestSuite(GGConversionTest.class);
+
+        //$JUnit-END$
+        return suite;
+    }
 }
--- test/pl/mn/communicator/gadu/GGConversionTest.java
+++ test/pl/mn/communicator/gadu/GGConversionTest.java
@@ -1,37 +1,38 @@
 package pl.mn.communicator.gadu;
 
+import junit.framework.TestCase;
+
 import org.apache.log4j.Logger;
 
 import pl.mn.communicator.gadu.util.Util;
 
-import junit.framework.TestCase;
 
 /**
  * @author mnaglik
  */
 public class GGConversionTest extends TestCase {
-	private static Logger logger = Logger.getLogger(GGConversionTest.class);
-	/*
-	 * Class to test for int byteToInt(byte[])
-	 */
-	public void testByteToIntbyteArray() {
-		byte [] bajty = new byte[] { 41,-54,26,0 };
-		int out = GGConversion.byteToInt(bajty);
-		logger.info("Bajty wejsciowe: "+Util.bytesToString(bajty));
-		logger.info("Wyjscie: "+out);		
-		assertEquals(out,1755689);
-	}
+    private static Logger logger = Logger.getLogger(GGConversionTest.class);
 
-	/*
-	 * Class to test for int byteToInt(byte[], int)
-	 */
-	public void testByteToIntbyteArrayint() {
-	}
+    /*
+     * Class to test for int byteToInt(byte[])
+     */
+    public void testByteToIntbyteArray() {
+        byte[] bajty = new byte[] { 41, -54, 26, 0 };
+        int out = GGConversion.byteToInt(bajty);
+        logger.info("Bajty wejsciowe: " + Util.bytesToString(bajty));
+        logger.info("Wyjscie: " + out);
+        assertEquals(out, 1755689);
+    }
 
-	public void testIntToByte() {
-	}
+    /*
+     * Class to test for int byteToInt(byte[], int)
+     */
+    public void testByteToIntbyteArrayint() {
+    }
 
-	public void testUnsignedIntToLong() {
-	}
+    public void testIntToByte() {
+    }
 
+    public void testUnsignedIntToLong() {
+    }
 }
