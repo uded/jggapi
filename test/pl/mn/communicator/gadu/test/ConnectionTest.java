9e78117543485639321f088916723534ce9e4d9c
Marcin Naglik <mnaglik@gazeta.pl>
2004-04-19 10:46:26 +0000

    *** empty log message ***

--- test/pl/mn/communicator/gadu/test/ConnectionTest.java
+++ test/pl/mn/communicator/gadu/test/ConnectionTest.java
@@ -3,10 +3,13 @@ package pl.mn.communicator.gadu.test;
 import java.io.IOException;
 
 import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+
+import pl.mn.communicator.ConnectionListener;
 import pl.mn.communicator.IConnection;
 import pl.mn.communicator.gadu.Connection;
 import pl.mn.communicator.gadu.LocalUser;
-import pl.mn.communicator.gadu.Message;
 import pl.mn.communicator.gadu.Server;
 
 /**
@@ -14,7 +17,7 @@ import pl.mn.communicator.gadu.Server;
  */
 public class ConnectionTest extends TestCase {
 	IConnection connection;
-
+	private static Logger log = Logger.getLogger(ConnectionTest.class);
 
 	public void testConnect() throws IOException {
 	}
@@ -23,12 +26,25 @@ public class ConnectionTest extends TestCase {
 		LocalUser user = new LocalUser(1336843,"dupadupa");
 		Server server = Server.getDefaultServer(user);
 		
-		System.out.println(server.getAddress()+":"+server.getPort());
+		log.info(server.getAddress()+":"+server.getPort());
 		
 		connection = new Connection(server,user);
-		connection.connect();
-		connection.sendMessage(new Message(1755689,"Test"));
-		connection.disconnect();
+		connection.addConnectionListener(new ConnectionListener() {
+
+			public void connectionEstablished() {
+				log.info("connection established");
+			}
+
+			public void disconnected() {
+                log.info("disconnected");
+			}
+
+			public void connectionError(String error) {
+                log.info("connection error: "+error);
+			}
+            
+        });
+        connection.connect();
 	}
 
 	public void testDisconnect() throws IOException {
