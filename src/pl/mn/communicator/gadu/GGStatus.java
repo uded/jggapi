eea796fb848cc8ca4fb501cb67eb82dfda1a6283
Marcin Naglik <mnaglik@gazeta.pl>
2004-04-21 15:15:42 +0000

    implementacja

--- src/pl/mn/communicator/gadu/GGStatus.java
+++ src/pl/mn/communicator/gadu/GGStatus.java
@@ -24,19 +24,41 @@ import pl.mn.communicator.IUser;
 
 /**
  * Pakiet powiadomienia u¿ytkownika o zmianie statusu u¿ytkownika z listy.
- * @version $Revision: 1.10 $
+ * @version $Revision: 1.11 $
  * @author mnaglik
  */
 class GGStatus implements GGIncomingPackage {
 	private static Logger logger = Logger.getLogger(GGStatus.class);
     private IUser user;
     private IStatus status;
+    private byte[] dane;
     
     public GGStatus(byte[] dane) {
     	logger.debug("Odebralem pakiet zmiany statusu uzytkownika");
+    	this.dane = dane;
+        analize();
     }
     
     /**
+	 * Analizuj pakiet przychodz±cy.
+	 */
+	private void analize() {
+		int userNo = GGConversion.byteToInt(dane);
+        logger.debug("Nr uzytkownika zmieniajacego status: "+userNo);
+        user = new User(userNo);
+        
+        int status = GGConversion.unsignedByteToInt(dane[4]);
+        logger.debug("Status u¿ytkownika to: " + status);
+
+        if ((status == GGNewStatus.GG_STATUS_AVAIL_DESCR) ||
+                (status == GGNewStatus.GG_STATUS_BUSY_DESCR) ||
+                (status == GGNewStatus.GG_STATUS_INVISIBLE_DESCR) ||
+                (status == GGNewStatus.GG_STATUS_NOT_AVAIL_DESCR)) {
+        	logger.debug("U¿ytkownik ma status opisowy");
+        }
+    }
+
+	/**
      * Pobierz u¿ytkonwika który zmieni³ status.
      * @return u¿ytkownik który zmieni³ status
      */
@@ -51,17 +73,19 @@ class GGStatus implements GGIncomingPackage {
     public IStatus getStatus() {
     	return status;
     }
+    
+    
     /*
     struct gg_status60 {
-        int uin;            // numer plus flagi w najstarszym bajcie //
-        char status;            // nowy stan //
-        int remote_ip;      // adres IP bezpo¶rednich po³±czeñ //
-        short remote_port;  // port bezpo¶rednich po³±czeñ //
-        char version;       // wersja klienta //
-        char image_size;    // maksymalny rozmiar grafiki //
-        char unknown1;      // 0x00 /
-        char description[]; // opis, nie musi wyst±piæ //
-        int time;       // czas, nie musi wyst±piæ //
+       4 int uin;            // numer plus flagi w najstarszym bajcie //
+       1 char status;            // nowy stan //
+       4 int remote_ip;      // adres IP bezpo¶rednich po³±czeñ //
+       2 short remote_port;  // port bezpo¶rednich po³±czeñ //
+       1 char version;       // wersja klienta //
+       1 char image_size;    // maksymalny rozmiar grafiki //
+       1 char unknown1;      // 0x00 /
+       1+char description[]; // opis, nie musi wyst±piæ //
+       4 int time;       // czas, nie musi wyst±piæ //
     };
     */
 }
