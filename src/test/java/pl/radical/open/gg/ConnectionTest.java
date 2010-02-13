79300714565786ab0a519eb493b2feca84b2b9b7
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-02-13 02:11:37 +0100

    Testing on the way

--- src/test/java/pl/radical/open/gg/ConnectionTest.java
+++ src/test/java/pl/radical/open/gg/ConnectionTest.java
@@ -3,6 +3,8 @@ package pl.radical.open.gg;
 import static org.junit.Assert.assertEquals;
 
 import pl.radical.open.gg.event.ConnectionListener;
+import pl.radical.open.gg.event.LoginFailedEvent;
+import pl.radical.open.gg.event.LoginListener;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -11,17 +13,68 @@ import org.slf4j.LoggerFactory;
 public class ConnectionTest {
 	private final Logger log = LoggerFactory.getLogger(getClass().getName());
 
+	public static boolean asyncOp = false;
+
+	private static ISession session1;
+	private static ISession session2;
+
 	@Test
 	public void connectionTest() throws GGException, InterruptedException {
 		log.info("Executing connectionTest() method");
 
-		final ISession session = SessionFactory.createSession();
-		final LoginContext loginContext = new LoginContext(20239471, "RadicalE");
+		session1 = connectUser(SessionFactory.createSession(), 20239471, "RadicalEntropy");
+		assertEquals(true, session1.getConnectionService().isConnected());
+
+		session2 = connectUser(SessionFactory.createSession(), 20241237, "Radical Test");
+		assertEquals(true, session2.getConnectionService().isConnected());
+	}
+
+	@Test
+	public void loginTest() throws InterruptedException {
+		ILoginService loginService;
+
+		log.debug("Loging in user1");
+		loginService = session1.getLoginService();
+		loginService.addLoginListener(new LoginListener.Stub() {
+			@Override
+			public void loginOK() {
+				log.info("Login for user 1 OK");
+			}
+
+			@Override
+			public void loginFailed(final LoginFailedEvent loginFailedEvent) {
+				log.error("Login failed!");
+			}
+		});
+
+		log.debug("Loging in user2");
+		loginService= session2.getLoginService();
+		loginService.addLoginListener(new LoginListener.Stub() {
+			@Override
+			public void loginOK() {
+				log.info("Login for user 2 OK");
+				asyncOp = true;
+			}
+
+			@Override
+			public void loginFailed(final LoginFailedEvent loginFailedEvent) {
+				log.error("Login failed!");
+			}
+		});
+
+		while(!asyncOp) {
+			Thread.sleep(100);
+		}
+	}
+
+	private ISession connectUser(final ISession session, final int uid, final String password) throws GGException, InterruptedException {
+		final LoginContext loginContext = new LoginContext(uid, password);
 
 		session.getConnectionService().addConnectionListener(new ConnectionListener.Stub() {
 			@Override
 			public void connectionEstablished() throws GGException {
 				log.info("Connection established.");
+				ConnectionTest.asyncOp = true;
 			}
 		});
 
@@ -29,10 +82,10 @@ public class ConnectionTest {
 		final IServer server = connectionService.lookupServer(loginContext.getUin());
 		connectionService.connect(server);
 
-		Thread.sleep(500);
-
-		assertEquals(true, connectionService.isConnected());
-
-		connectionService.disconnect();
+		while (!asyncOp) {
+			Thread.sleep(100);
+		}
+		asyncOp = false;
+		return session;
 	}
 }
