e52ecbae36fb4cc8d04131d04cacc92ea85342ed
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-02-21 23:34:44 +0100

    Update version of GG protocol

--- src/main/java/pl/radical/open/gg/packet/GGVersion.java
+++ src/main/java/pl/radical/open/gg/packet/GGVersion.java
@@ -6,32 +6,30 @@ package pl.radical.open.gg.packet;
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
  * @author <a href="mailto:lukasz.rzanek@radical.com.pl>Łukasz Rżanek</a>
  */
-public interface GGVersion {
-
-	int VERSION_80_build_8283 = 0x2E;
-	int VERSION_80_build_4881 = 0x2D;
-	int VERSION_77_build_3315 = 0x2A;
-	int VERSION_76_build_1688 = 0x29;
-	int VERSION_7_5_0_build_2201 = 0x28;
-	int VERSION_70_build_22 = 0x27;
-	int VERSION_70_build_20 = 0x26;
-	int VERSION_70_build_1 = 0x25;
-	int VERSION_60_1_build_155 = 0x24; // 7.6 (1359)
-	int VERSION_60_1_build_140 = 0x22;
-	int VERSION_60_1_build_133 = 0x21;
-	int VERSION_60 = 0x20;
-	int VERSION_5_7_beta_build_121 = 0x1E;
-	int VERSION_5_6_beta = 0x1C;
-	int VERSION_5_0_5 = 0x1B;
-	int VERSION_5_0_3 = 0x19;
-	int VERSION_5_0_0 = 0x18; // 4.9.3, 5.0.0, 5.0.1
-	int VERSION_4_9_2 = 0x17;
-	int VERSION_4_9_1 = 0x16;
-	int VERSION_4_8_9 = 0x15;
-	int VERSION_4_8_3 = 0x14; // 4.8.3, 4.8.1
-	int VERSION_4_6_10 = 0x11; // 4.6.10, 4.6.1
-	int VERSION_4_5_22 = 0x10; // 4.5.22, 4.5.21, 4.5.19, 4.5.17, 4.5.15
-	int VERSION_4_5_12 = 0x0F; // 4.5.12
-	int VERSION_4_0_30 = 0x0B; // 4.0.30, 4.0.29, 4.0.28, 4.0.25
-
+public final class GGVersion {
+	public static final int VERSION_80_build_8283 = 0x2E;
+	public static final int VERSION_80_build_4881 = 0x2D;
+	public static final int VERSION_77_build_3315 = 0x2A;
+	public static final int VERSION_76_build_1688 = 0x29;
+	public static final int VERSION_7_5_0_build_2201 = 0x28;
+	public static final int VERSION_70_build_22 = 0x27;
+	public static final int VERSION_70_build_20 = 0x26;
+	public static final int VERSION_70_build_1 = 0x25;
+	public static final int VERSION_60_1_build_155 = 0x24; // 7.6 (1359)
+	public static final int VERSION_60_1_build_140 = 0x22;
+	public static final int VERSION_60_1_build_133 = 0x21;
+	public static final int VERSION_60 = 0x20;
+	public static final int VERSION_5_7_beta_build_121 = 0x1E;
+	public static final int VERSION_5_6_beta = 0x1C;
+	public static final int VERSION_5_0_5 = 0x1B;
+	public static final int VERSION_5_0_3 = 0x19;
+	public static final int VERSION_5_0_0 = 0x18; // 4.9.3, 5.0.0, 5.0.1
+	public static final int VERSION_4_9_2 = 0x17;
+	public static final int VERSION_4_9_1 = 0x16;
+	public static final int VERSION_4_8_9 = 0x15;
+	public static final int VERSION_4_8_3 = 0x14; // 4.8.3, 4.8.1
+	public static final int VERSION_4_6_10 = 0x11; // 4.6.10, 4.6.1
+	public static final int VERSION_4_5_22 = 0x10; // 4.5.22, 4.5.21, 4.5.19, 4.5.17, 4.5.15
+	public static final int VERSION_4_5_12 = 0x0F; // 4.5.12
+	public static final int VERSION_4_0_30 = 0x0B; // 4.0.30, 4.0.29, 4.0.28, 4.0.25
 }
