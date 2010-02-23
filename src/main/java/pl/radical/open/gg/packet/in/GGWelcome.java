64b4dbaa0e59dc0e2d37c488125f0c716dde208f
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-02-23 01:17:21 +0100

    Extending logging

--- src/main/java/pl/radical/open/gg/packet/in/GGWelcome.java
+++ src/main/java/pl/radical/open/gg/packet/in/GGWelcome.java
@@ -1,7 +1,9 @@
 package pl.radical.open.gg.packet.in;
 
-import pl.radical.open.gg.GGNullPointerException;
 import pl.radical.open.gg.packet.GGUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The packet is retrieved from the Gadu-Gadu server just after we connect to it. The class parses package and gets seed
@@ -9,8 +11,10 @@ import pl.radical.open.gg.packet.GGUtils;
  * 
  * @author <a href="mailto:mnaglik@gazeta.pl">Marcin Naglik</a>
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
+ * @author <a href="mailto:lukasz.rzanek@radical.com.pl>Łukasz Rżanek</a>
  */
 public class GGWelcome implements GGIncomingPackage {
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	public static final int GG_PACKAGE_WELCOME = 0x1;
 
@@ -18,7 +22,11 @@ public class GGWelcome implements GGIncomingPackage {
 
 	public GGWelcome(final byte[] data) {
 		if (data == null) {
-			throw new GGNullPointerException("data cannot be null");
+			log.error("Was expecting a data in constructor, got null");
+			throw new IllegalArgumentException("data cannot be null");
+		}
+		if (log.isTraceEnabled()) {
+			log.trace("Seed to be used in this connection: {}", m_seed);
 		}
 		m_seed = GGUtils.byteToInt(data);
 	}
