8e8a8b62a15e130465b066ad7ed229b01f2b7544
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-03-14 23:59:57 +0100

    Moving things around, never ending story

---
+++ src/main/java/pl/radical/open/gg/packet/GGIncomingPackage.java
@@ -0,0 +1,11 @@
+package pl.radical.open.gg.packet;
+
+
+/**
+ * Incoming Gadu-Gadu packet.
+ * 
+ * @author <a href="mailto:mnaglik@gazeta.pl">Marcin Naglik</a>
+ */
+public interface GGIncomingPackage extends GGPacket {
+
+}
