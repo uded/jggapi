2fef50f1186d80f82129373e12b580e7c12b5021
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-03-14 23:56:18 +0100

    Moving things aroud

--- src/main/java/pl/radical/open/gg/packet/GGOutgoingPackage.java
+++ src/main/java/pl/radical/open/gg/packet/GGOutgoingPackage.java
@@ -1,10 +1,9 @@
-package pl.radical.open.gg.packet.out;
-
-import pl.radical.open.gg.packet.GGPacket;
+package pl.radical.open.gg.packet;
 
 /**
  * @author <a href="mailto:mnaglik@gazeta.pl">Marcin Naglik</a>
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
+ * @author <a href="mailto:lukasz.rzanek@radical.com.pl>Łukasz Rżanek</a>
  */
 public interface GGOutgoingPackage extends GGPacket {
 
