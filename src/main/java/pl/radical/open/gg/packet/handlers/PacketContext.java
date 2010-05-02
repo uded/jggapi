5b3de2a3e94de85927544f20576b4ce55e8cbd5e
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2010-05-02 23:04:03 +0200

    Small cleanup - "m_*" pattern for local variables, it will take a while

--- src/main/java/pl/radical/open/gg/packet/handlers/PacketContext.java
+++ src/main/java/pl/radical/open/gg/packet/handlers/PacketContext.java
@@ -1,9 +1,10 @@
 package pl.radical.open.gg.packet.handlers;
 
-import pl.radical.open.gg.GGNullPointerException;
 import pl.radical.open.gg.Session;
 import pl.radical.open.gg.Session.SessionAccessor;
 import pl.radical.open.gg.packet.GGHeader;
+
+import java.security.InvalidParameterException;
 
 /**
  * Created on 2004-11-27
@@ -11,28 +12,27 @@ import pl.radical.open.gg.packet.GGHeader;
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
  */
 public class PacketContext {
-
-	private Session m_session = null;
-	private GGHeader m_header = null;
-	private byte[] m_packageContent = null;
+	private Session session = null;
+	private GGHeader header = null;
+	private byte[] packageContent = null;
 
 	public PacketContext(final Session session, final GGHeader header, final byte[] packageContent) {
 		if (session == null) {
-			throw new GGNullPointerException("session cannot be null");
+			throw new InvalidParameterException("session cannot be null");
 		}
 		if (header == null) {
-			throw new GGNullPointerException("header cannot be null");
+			throw new InvalidParameterException("header cannot be null");
 		}
 		if (packageContent == null) {
-			throw new GGNullPointerException("packageContent cannot be null");
+			throw new InvalidParameterException("packageContent cannot be null");
 		}
-		m_session = session;
-		m_header = header;
-		m_packageContent = packageContent;
+		this.session = session;
+		this.header = header;
+		this.packageContent = packageContent;
 	}
 
 	public SessionAccessor getSessionAccessor() {
-		return m_session.getSessionAccessor();
+		return session.getSessionAccessor();
 	}
 
 	/**
@@ -41,7 +41,7 @@ public class PacketContext {
 	 * @return Gadu-Gadu packet header.
 	 */
 	public GGHeader getHeader() {
-		return m_header;
+		return header;
 	}
 
 	/**
@@ -50,7 +50,7 @@ public class PacketContext {
 	 * @return Gadu-Gadu packet content
 	 */
 	public byte[] getPackageContent() {
-		return m_packageContent;
+		return packageContent;
 	}
 
 }
