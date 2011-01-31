64983955c68ec2453db5b764f0aa3546d9c7f925
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2011-01-31 06:09:27 +0100

    Got rid of getters and toString

--- src/main/java/pl/radical/open/gg/packet/GGHeader.java
+++ src/main/java/pl/radical/open/gg/packet/GGHeader.java
@@ -2,22 +2,28 @@ package pl.radical.open.gg.packet;
 
 import pl.radical.open.gg.utils.GGUtils;
 
+import lombok.Getter;
+import lombok.ToString;
+
 /**
  * Gadu-Gadu packet header.
  * 
  * @author <a href="mailto:mnaglik@gazeta.pl">Marcin Naglik</a>
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
  */
+@ToString
 public class GGHeader {
 
 	/**
 	 * Gadu-Gadu packet type
 	 */
+	@Getter
 	private int type = -1;
 
 	/**
 	 * Gadu-Gadu packet length
 	 */
+	@Getter
 	private int length = -1;
 
 	/**
@@ -29,32 +35,6 @@ public class GGHeader {
 	public GGHeader(final byte[] data) {
 		type = GGUtils.byteToInt(data, 0);
 		length = GGUtils.byteToInt(data, 4);
-	}
-
-	/**
-	 * Returns the type.
-	 * 
-	 * @return int packet type.
-	 */
-	public int getType() {
-		return type;
-	}
-
-	/**
-	 * Returns the length of packet.
-	 * 
-	 * @return int packet length.
-	 */
-	public int getLength() {
-		return length;
-	}
-
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "[PacketType: " + type + ", packetLength: " + length + "]";
 	}
 
 }
