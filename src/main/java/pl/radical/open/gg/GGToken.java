d87db2f1798633c6368dce5d5bece9ff7d13041d
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2011-01-31 05:57:20 +0100

    Some more cleaning up

--- src/main/java/pl/radical/open/gg/GGToken.java
+++ src/main/java/pl/radical/open/gg/GGToken.java
@@ -1,75 +1,40 @@
 package pl.radical.open.gg;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 /**
- * Created on 2005-01-26
- * 
  * @author <a href="mailto:mati@sz.home.pl">Mateusz Szczap</a>
+ * @author <a href="mailto:lukasz.rzanek@radical.com.pl">Łukasz Rżanek</a>
+ * @since 2005-01-26
  */
+@Data
+// FIXME Seriously?
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class GGToken {
 
 	/** the width of a picture */
+	@NonNull
 	private int imageWidth;
 
 	/** the height of a picture */
+	@NonNull
 	private int imageHeight;
 
 	/** the length of token */
+	@NonNull
 	private int tokenLength;
 
 	/** the id of token */
+	@NonNull
 	private String tokenID;
 
 	/** the token URL */
+	@NonNull
 	private String tokenURL;
-
-	/**
-	 * @return Returns the imageWidth.
-	 */
-	public int getImageWidth() {
-		return imageWidth;
-	}
-
-	public void setImageWidth(final int imageWidth) {
-		if (imageWidth < 0) {
-			throw new IllegalArgumentException("imageWidth cannot be null");
-		}
-		this.imageWidth = imageWidth;
-	}
-
-	public int getImageHeight() {
-		return imageHeight;
-	}
-
-	public void setImageHeight(final int imageHeight) {
-		if (imageHeight < 0) {
-			throw new IllegalArgumentException("imageHeight cannot be null");
-		}
-		this.imageHeight = imageHeight;
-	}
-
-	public String getTokenURL() {
-		return tokenURL;
-	}
-
-	public void setTokenURL(final String tokenURL) {
-		this.tokenURL = tokenURL;
-	}
-
-	public String getTokenID() {
-		return tokenID;
-	}
-
-	public void setTokenID(final String tokenID) {
-		this.tokenID = tokenID;
-	}
-
-	public int getTokenLength() {
-		return tokenLength;
-	}
-
-	public void setTokenLength(final int tokenLength) {
-		this.tokenLength = tokenLength;
-	}
 
 	public String getFullTokenURL() {
 		final StringBuffer buffer = new StringBuffer();
